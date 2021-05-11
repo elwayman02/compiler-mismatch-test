ccadcb10ccd143715d72c8fc3b61b001d733ea83
Jordan Hawker <hawker.jordan@gmail.com>
2021-05-11 16:55:14 -0700

    Remove ember-exam
    * ember-try should now call `yarn test:ember` directly

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,11 +1,8 @@
 'use strict';
 
-const getChannelURL = require('ember-source-channel-url');
-const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
-
 module.exports = async function () {
   return {
-    command: 'yarn test',
+    command: 'yarn test:ember',
     useYarn: true,
     scenarios: [
       {
@@ -25,59 +22,11 @@ module.exports = async function () {
         },
       },
       {
-        name: 'ember-release',
+        name: 'latest (default)',
         npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('release'),
-          },
+          devDependencies: {},
         },
       },
-      {
-        name: 'ember-beta',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('beta'),
-          },
-        },
-      },
-      {
-        name: 'ember-canary',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('canary'),
-          },
-        },
-      },
-      {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true,
-          }),
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^1.1.0',
-          },
-        },
-      },
-      {
-        name: 'ember-classic',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'application-template-wrapper': true,
-            'default-async-observers': false,
-            'template-only-glimmer-components': false,
-          }),
-        },
-        npm: {
-          ember: {
-            edition: 'classic',
-          },
-        },
-      },
-      embroiderSafe(),
-      embroiderOptimized(),
     ],
   };
 };
